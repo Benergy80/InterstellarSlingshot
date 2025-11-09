4555e4e573ad813a61d84a205c13faa708d2a49b
Ben <BENJAMINSTAGL@GMAIL.COM>
2025-11-09 23:24:39 +0000

    Update game-objects.js

--- js/game-objects.js
+++ js/game-objects.js
@@ -5625,11 +5625,11 @@ function updateHubbleSkybox2Opacity() {
     if (distanceFromStart < fadeStartDistance) {
         targetOpacity = 0.00;
     } else if (distanceFromStart > fadeEndDistance) {
-        targetOpacity = .5;
+        targetOpacity = .6;
     } else {
         // Linear interpolation between 0.00 and 0.02
         const progress = (distanceFromStart - fadeStartDistance) / (fadeEndDistance - fadeStartDistance);
-        targetOpacity = 0.00 + (progress * .5); // 0.02 = 0.02 - 0.00
+        targetOpacity = 0.00 + (progress * .6); // 0.02 = 0.02 - 0.00
     }
     
     // Smoothly transition to target opacity
