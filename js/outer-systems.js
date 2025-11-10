102f2936d47b3bd1f99a4f2298aeda04815c9867
Ben <BENJAMINSTAGL@GMAIL.COM>
2025-11-10 00:04:57 +0000

    Update outer-systems.js

--- js/outer-systems.js
+++ js/outer-systems.js
@@ -559,7 +559,7 @@ function updateOuterSystems() {
         if (!system.userData || !system.userData.orbiters) return;
         
         const systemDist = system.position.distanceTo(playerPos);
-        const blurStart = 40000;
+        const blurStart = 60000;
         const blurMax = 80000;
         
         let opacity = 1.0;
